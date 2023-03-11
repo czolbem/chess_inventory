31a4bfb90215c9f3ecb02609a18b6293d3a5976f
Marc Czolbe <czolbe@hotmail.de>
2023-03-11 16:45:48 +0000

    * Add base template and related resources * Add Home view

--- inventory/tests/test_views.py
+++ inventory/tests/test_views.py
@@ -1,3 +1,13 @@
+from http import HTTPStatus
+
 from django.test import TestCase
 
-# Create your tests here.
+from django.urls import reverse
+
+
+class ViewTests(TestCase):
+    def test_get_home_returns_200(self):
+        url = reverse('home')
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, HTTPStatus.OK)
