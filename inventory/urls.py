e0529abc2d60c61d54ba989d7157b39c97aa65f9
Marc Czolbe <czolbe@hotmail.de>
2023-03-11 19:27:57 +0000

    * Add detail view for game

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from inventory.views import HomeView, GameFormView, GameListView
+from inventory.views import HomeView, GameFormView, GameListView, GameDetailView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('add', GameFormView.as_view(), name='add_game'),
     path('games', GameListView.as_view(), name='games'),
+    path('game/<int:pk>', GameDetailView.as_view(), name='game'),
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -1,5 +1,6 @@
-from django.urls import reverse_lazy
-from django.views.generic import TemplateView, FormView, ListView
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views.generic import TemplateView, FormView, ListView, DetailView
 
 from inventory.forms import GameForm
 from inventory.models import Game
@@ -12,14 +13,17 @@ class HomeView(TemplateView):
 class GameFormView(FormView):
     template_name = "game_form.html"
     form_class = GameForm
-    # TODO change this to something better
-    success_url = reverse_lazy('home')
 
-    def form_valid(self, form):
-        form.save()
-        return super(GameFormView, self).form_valid(form)
+    def form_valid(self, form) -> HttpResponseRedirect:
+        game = form.save()
+        return HttpResponseRedirect(reverse('game', kwargs={'pk': game.pk}))
 
 
 class GameListView(ListView):
     model = Game
     template_name = "game_list.html"
+
+
+class GameDetailView(DetailView):
+    model = Game
+    template_name = "game_detail.html"
